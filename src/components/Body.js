2351cca47f04f3bdf99a146f21ee60485654891f
Ajinkya Pathare <78788818+apathare9@users.noreply.github.com>
2024-02-23 16:53:29 +0530

    ...

--- src/components/Body.js
+++ src/components/Body.js
@@ -33,9 +33,9 @@ const Body = () => {
     const data = await fetch(
       `https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2181987&lng=72.9622965&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
     );
-    const response = await fetch(data);
+    // const response = await fetch(data);
     console.log("ABCD");
-    console.log(response);
+    // console.log(response);
 
     const json = await data.json();
     console.log(json);
